884ad3b77d5c603b22fe7e666dc2b68b92e1f503
Bryson LeBlanc <bwl0016@auburn.edu>
2025-11-03 11:33:03 -0600

    render root properly and connect to App.js

--- js/src/App.js
+++ js/src/App.js
@@ -1,24 +1,17 @@
-
-window.location.href = '/index.html';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default function App() {
+    console.log("App component rendered");
+    return (
+        <div className="App">
+        <header className="App-header">
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </header>
+        </div>
+    );
+}
