ba4ce2b7cefe053917fb82bfab3731cbf4ce8caa
Bryson LeBlanc <bwl0016@auburn.edu>
2025-11-03 12:48:03 -0600

    calendar, unstyled

--- js/src/App.js
+++ js/src/App.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+import { MiniCalendar } from '@mantine/dates';
+import { MantineProvider, createTheme } from '@mantine/core';
+
 export default function App() {
     console.log("App component rendered");
     return (
-        <div className="App">
-        <header className="App-header">
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-            </a>
-        </header>
-        </div>
+        <MantineProvider>
+            <div className="App">
+                <header className="App-header">
+                    <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Learning React
+                    </a>
+                </header>
+                <MyMiniCalendar />
+            </div>
+        </MantineProvider>
+    );
+}
+
+function MyMiniCalendar() {
+    const [selectedDate, setSelectedDate] = useState(null);
+    return (
+        <MiniCalendar value={selectedDate} onChange={setSelectedDate} numberOfDays={7} />
     );
 }
